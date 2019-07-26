fd243141ed17c68ef82709bbf9e98aff41c3d58e
R0H17 <r0h17@purpe.com>
2019-07-26 12:08:10 +0530

    SKEL update: WIP

--- sw/vhdl/SKEL/skel.vhd
+++ sw/vhdl/SKEL/skel.vhd
@@ -32,11 +32,12 @@ architecture synth of %%SKEL_PRJNAME%% is
         signal reset_n:         std_logic;
 begin
         led <= '0';
-        gpio <= (others => '0');
+        gpio <= (others => 'Z');
         spi0_sck <= '0';
         spi0_ss <= '0';
         spi0_mosi <= '0';
         sdram_addr <= (others => '0');
+        sdram_data <= (others => 'Z');
         sdram_ba <= (others => '0');
         sdram_dqm <= (others => '0');
         sdram_ras <= '1';
