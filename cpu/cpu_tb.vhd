016d397ff7bc0a0f1fec4e3977c414191b651905
R0H17 <r0h17@purpe.com>
2019-09-16 14:52:00 +0530

    sdram_controller WIP

--- cpu/cpu_tb.vhd
+++ cpu/cpu_tb.vhd
@@ -32,31 +32,44 @@ architecture sim of cpu_tb is
 begin
         clk_50m <= not clk_50m after half_period;
 
+        sdram: entity work.sim_sdram_mt48lc16m16a2
+            port map(
+                dq => sdram_data,
+                dqm => sdram_dqm,
+                addr => sdram_addr,
+                ba => sdram_ba,
+                clk => sdram_clk,
+                cke => sdram_cke,
+                cs_n => sdram_cs,
+                ras_n => sdram_ras,
+                cas_n => sdram_cas,
+                we_n => sdram_we);
+
         cpu: entity work.cpu
-                port map(
-                        clk_50m => clk_50m,
-                        btn => btn,
-                        led => led,
-
-                        -- uart
-                        -- gpio
-
-                        -- spio_*
-                        spi0_ss => spi0_ss,
-                        spi0_sck => spi0_sck,
-                        spi0_mosi => spi0_mosi,
-                        spi0_miso => spi0_miso,
-
-                        sdram_data => sdram_data,
-                        sdram_addr => sdram_addr,
-                        sdram_ba => sdram_ba,
-                        sdram_dqm => sdram_dqm,
-                        sdram_ras => sdram_ras,
-                        sdram_cas => sdram_cas,
-                        sdram_cke => sdram_cke,
-                        sdram_clk => sdram_clk,
-                        sdram_cs => sdram_cs,
-                        sdram_we => sdram_we
-                );
+            port map(
+                clk_50m => clk_50m,
+                btn => btn,
+                led => led,
+                
+                -- uart
+                -- gpio
+                
+                -- spio_*
+                spi0_ss => spi0_ss,
+                spi0_sck => spi0_sck,
+                spi0_mosi => spi0_mosi,
+                spi0_miso => spi0_miso,
+                
+                sdram_data => sdram_data,
+                sdram_addr => sdram_addr,
+                sdram_ba => sdram_ba,
+                sdram_dqm => sdram_dqm,
+                sdram_ras => sdram_ras,
+                sdram_cas => sdram_cas,
+                sdram_cke => sdram_cke,
+                sdram_clk => sdram_clk,
+                sdram_cs => sdram_cs,
+                sdram_we => sdram_we
+            );
                         
 end architecture;
--- cpu/sdram_controller.vhd
+++ cpu/sdram_controller.vhd
@@ -59,8 +59,9 @@ architecture synth of sdram_controller is
     signal addr:        std_logic_vector(12 downto 0) := LMR_SETTING;                                
     signal ba:          std_logic_vector(1 downto 0) := (others => '0');
     signal dqm:         std_logic_vector(1 downto 0) := (others => '1');
+    signal busy_wait:   std_logic_vector(3 downto 0) := (others => '0');
 
-    signal cycle_counter:       bit_vector(STARTUP_CYCLE_BITNR downto 0) := 
+    signal cycle_counter:       std_logic_vector(STARTUP_CYCLE_BITNR downto 0) := 
                                                             (others => '0');
 
 begin
@@ -78,6 +79,40 @@ begin
     sdram_data <= (others => 'Z');
 
     mc_out.op_strobe <= '0';
+
+    process(sys_clk)
+    begin
+        if (rising_edge(sys_clk)) then
+            cycle_counter <= std_logic_vector(unsigned(cycle_counter) + 1);
+            case state is
+                when STARTUP =>
+                    if (cycle_counter(STARTUP_CYCLE_BITNR) = '1') then
+                        cke <= '1';
+                        command <= CMD_NOP;
+                        if (cycle_counter(2 downto 0) = "001") then
+                            command <= CMD_PRECHARGE;
+                        elsif (cycle_counter(2 downto 0) = "101") then
+                            command <= CMD_LMR;
+                        elsif (cycle_counter(2 downto 0) = "111") then
+                            state <= IDLE;
+                        end if;
+                    end if;
+                when IDLE =>
+                    command <= CMD_NOP;
+                    if (busy_wait = "0000") then
+                        if (unsigned(cycle_counter) > REFRESH_CYCLES) then
+                            busy_wait <= "0" & TRFC_CYCLES;
+                            cycle_counter <= 
+                                std_logic_vector(unsigned(cycle_counter) - 
+                                                                REFRESH_CYCLES);
+                            command <= CMD_REFRESH;
+                        end if;
+                    else
+                        busy_wait <= std_logic_vector(unsigned(busy_wait) - 1);
+                    end if;
+            end case;
+        end if;
+    end process;
 end architecture;
 
 
