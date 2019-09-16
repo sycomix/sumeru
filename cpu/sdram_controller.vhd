0145f8acae1d541b6ed8f3bd14116fa12df0601a
R0H17 <r0h17@purpe.com>
2019-09-16 12:36:34 +0530

    sdram_controller w WIP

--- cpu/sdram_controller.vhd
+++ cpu/sdram_controller.vhd
@@ -1,10 +1,22 @@
 library ieee;
 use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.ALL;
+use work.cpu_types.ALL;
 use work.memory_channel_types.ALL;
 
 entity sdram_controller is
-port(
+    generic(
+        -- One refresh command every 7.813us (or 7813 ns)
+        -- (7812.5 / SYS_CLK_PERIOD_NS) - 7.0;
+        REFRESH_CYCLES:         natural := 1035;
+        TRFC_CYCLES:            std_logic_vector(2 downto 0) := "111";
+        TRP_CYCLES:             std_logic_vector(2 downto 0) := "010";
+        TRCD_CYCLES:            std_logic_vector(2 downto 0) := "010";
+        CAS_CYCLES:             std_logic_vector(2 downto 0) := "001";
+
+        STARTUP_CYCLE_BITNR:    natural := 14
+    );
+    port(
         sys_clk:                in std_logic;
         mem_clk:                in std_logic;
         mc_in:                  in mem_channel_in_t;
@@ -24,6 +36,9 @@ port(
 end entity;
 
 architecture synth of sdram_controller is
+    signal cycle_counter:       bit_vector(STARTUP_CYCLE_BITNR downto 0) := 
+                                                            (others => '0');
+
 begin
     sdram_addr <= (others => '0');
     sdram_data <= (others => 'Z');
