3283ea569e03105ae096fe6d8cb058bb703ee2e3
R0H17 <r0h17@purpe.com>
2019-09-23 12:07:33 +0530

    Memory loader WIP

---
+++ cpu/memory_loader.vhd
@@ -0,0 +1,63 @@
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.numeric_std.ALL;
+use work.memory_channel_types.ALL;
+
+entity memory_loader is
+port(
+        sys_clk:                in std_logic;
+        mem_clk:                in std_logic;
+        reset_n:                in std_logic;
+        load_done:              out std_logic;
+        mc_in:                  out mem_channel_in_t;
+        mc_out:                 in mem_channel_out_t
+    );
+end entity;
+
+architecture synth of memory_loader is
+    signal counter:             std_logic_vector(9 downto 0) := (others => '0');
+    signal rom_data:            std_logic_vector(15 downto 0);
+    signal op_start:            std_logic := '0';
+
+    type loader_state_t is (
+        IDLE,
+        DONE);
+
+    signal state:               loader_state_t := IDLE;
+
+
+begin
+    load_done <= counter(9);
+    mc_in.op_addr <= "000000000000000" & counter(8 downto 0);
+    mc_in.op_start <= op_start;
+    mc_in.op_wren <= '1';
+    mc_in.op_burst <= '0';
+    mc_in.op_dqm <= "00";
+    mc_in.write_data <= rom_data;
+
+    rom: entity work.alt_rom
+        generic map(
+            AWIDTH => 9,
+            DWIDTH => 16,
+            DATA_FILE => "BOOTCODE.mif")
+        port map(
+            clock => sys_clk,
+            address => counter(8 downto 0),
+            q => rom_data);
+
+    process(sys_clk)
+    begin
+        if (rising_edge(sys_clk) and reset_n = '1') then
+            case state is
+                when IDLE =>
+                    if (load_done = '1') then
+                        state <= DONE;
+                    end if;
+                when DONE =>
+            end case;
+        end if;
+    end process;
+
+end architecture;
+
+
---
+++ sw/vhdl/libsrc/alt_rom.vhd
@@ -0,0 +1,54 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+LIBRARY altera_mf;
+USE altera_mf.altera_mf_components.all;
+
+ENTITY alt_rom IS
+        GENERIC
+        (
+                AWIDTH          : INTEGER;
+                DWIDTH          : INTEGER;
+                DATA_FILE       : STRING
+        );
+	PORT
+	(
+		address		: IN STD_LOGIC_VECTOR ((AWIDTH - 1) DOWNTO 0);
+		clock		: IN STD_LOGIC  := '1';
+		q		: OUT STD_LOGIC_VECTOR ((DWIDTH - 1) DOWNTO 0)
+	);
+END alt_rom;
+
+
+ARCHITECTURE SYN OF alt_rom IS
+
+	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (15 DOWNTO 0);
+
+BEGIN
+	q    <= sub_wire0(15 DOWNTO 0);
+
+	altsyncram_component : altsyncram
+	GENERIC MAP (
+		address_aclr_a => "NONE",
+		clock_enable_input_a => "BYPASS",
+		clock_enable_output_a => "BYPASS",
+		init_file => DATA_FILE,
+		intended_device_family => "Cyclone IV E",
+		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
+		lpm_type => "altsyncram",
+		numwords_a => 512,
+		operation_mode => "ROM",
+		outdata_aclr_a => "NONE",
+		outdata_reg_a => "UNREGISTERED",
+		ram_block_type => "M9K",
+		widthad_a => AWIDTH,
+		width_a => DWIDTH,
+		width_byteena_a => 1
+	)
+	PORT MAP (
+		address_a => address,
+		clock0 => clock,
+		q_a => sub_wire0
+	);
+
+END SYN;
