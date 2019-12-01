e61b00099ed49bed6192b4675ce211b26a41ceb6
R0H17 <r0h17@purpe.com>
2019-12-01 13:56:47 +0530

    WIP

--- cpu/cpu.vhd
+++ cpu/cpu.vhd
@@ -69,109 +69,109 @@ architecture synth of cpu is
     signal state:               state_t := S1;
 
 begin
-    spi0_sck <= '0';
-    spi0_ss <= '0';
-    spi0_mosi <= '0';
+spi0_sck <= '0';
+spi0_ss <= '0';
+spi0_mosi <= '0';
 
-    pll: entity work.pll 
-        port map(
-            inclk0 => clk_50m,
-            c0 => sys_clk,
-            c1 => mem_clk,
-            locked => reset_n);
+pll: entity work.pll 
+    port map(
+        inclk0 => clk_50m,
+        c0 => sys_clk,
+        c1 => mem_clk,
+        locked => reset_n);
 
-    sdram_controller: entity work.sdram_controller
-        port map(
-            sys_clk => sys_clk,
-            mem_clk => mem_clk,
-            mc_in => sdc_in,
-            mc_out => sdc_out,
-            data_out => sdc_data_out,
-            sdram_data => sdram_data,
-            sdram_addr => sdram_addr,
-            sdram_ba => sdram_ba,
-            sdram_dqm => sdram_dqm,
-            sdram_ras => sdram_ras,
-            sdram_cas => sdram_cas,
-            sdram_cke => sdram_cke,
-            sdram_clk => sdram_clk,
-            sdram_we => sdram_we,
-            sdram_cs => sdram_cs,
-            busy => sdc_busy);
-            
-    memory_arbitrator: entity work.memory_arbitrator
-        port map(
-            clk => sys_clk,
+sdram_controller: entity work.sdram_controller
+    port map(
+        sys_clk => sys_clk,
+        mem_clk => mem_clk,
+        mc_in => sdc_in,
+        mc_out => sdc_out,
+        data_out => sdc_data_out,
+        sdram_data => sdram_data,
+        sdram_addr => sdram_addr,
+        sdram_ba => sdram_ba,
+        sdram_dqm => sdram_dqm,
+        sdram_ras => sdram_ras,
+        sdram_cas => sdram_cas,
+        sdram_cke => sdram_cke,
+        sdram_clk => sdram_clk,
+        sdram_we => sdram_we,
+        sdram_cs => sdram_cs,
+        busy => sdc_busy);
+        
+memory_arbitrator: entity work.memory_arbitrator
+    port map(
+        clk => sys_clk,
 
-            sdc_busy => sdc_busy,
-            sdc_in => sdc_in,
-            sdc_out => sdc_out,
+        sdc_busy => sdc_busy,
+        sdc_in => sdc_in,
+        sdc_out => sdc_out,
 
-            mc0_in => mc0_in,
-            mc0_out => mc0_out,
+        mc0_in => mc0_in,
+        mc0_out => mc0_out,
 
-            mc1_in => mc1_in,
-            mc1_out => mc1_out,
+        mc1_in => mc1_in,
+        mc1_out => mc1_out,
 
-            mc2_in => mc2_in,
-            mc2_out => mc2_out,
+        mc2_in => mc2_in,
+        mc2_out => mc2_out,
 
-            mc3_in => mc3_in,
-            mc3_out => mc3_out
-        );
+        mc3_in => mc3_in,
+        mc3_out => mc3_out
+    );
 
-    mc1_in <= bc_mc1_in when bootcode_load_done = '0' else pbus_mc1_in;
+mc1_in <= bc_mc1_in when bootcode_load_done = '0' else pbus_mc1_in;
 
-    bootcode_loader: entity work.memory_loader
+bootcode_loader: entity work.memory_loader
         generic map(
-            DATA_FILE => "BOOTCODE.hex"
-        )
-        port map(
-            sys_clk => sys_clk,
-            mem_clk => mem_clk,
-            reset_n => reset_n,
+        DATA_FILE => "BOOTCODE.hex"
+    )
+    port map(
+        sys_clk => sys_clk,
+        mem_clk => mem_clk,
+        reset_n => reset_n,
 
-            load_done => bootcode_load_done,
+        load_done => bootcode_load_done,
             mc_in => bc_mc1_in,
             mc_out => mc1_out);
 
-    icache: entity work.icache
-        port map(
-            sys_clk => sys_clk,
-            cache_clk => mem_clk,
-            addr => pc(24 downto 0),
-            meta => icache_meta,
-            data => icache_data,
+icache: entity work.icache
+    port map(
+        sys_clk => sys_clk,
+        cache_clk => mem_clk,
+        addr => pc(24 downto 0),
+        meta => icache_meta,
+        data => icache_data,
             load => icache_load,
-            flush => '0',
-            -- flush_strobe =>
-            mc_in => mc0_in,
-            mc_out => mc0_out,
-            sdc_data_out => sdc_data_out);
+        flush => '0',
+        -- flush_strobe =>
+        mc_in => mc0_in,
+        mc_out => mc0_out,
+        sdc_data_out => sdc_data_out);
 
-    process(sys_clk)
-    begin
-        if (rising_edge(sys_clk)) then
-            case state is
-                when S1 =>
-                    if (bootcode_load_done = '1') then
-                        icache_load <= '1';
-                        state <= S2;
+process(sys_clk)
+begin
+    if (rising_edge(sys_clk)) then
+        case state is
+            when S1 =>
+                if (bootcode_load_done = '1') then
+                    icache_load <= '1';
+                    state <= S2;
+                end if;
+            when S2 =>
+                icache_load <= '0';
+                if (icache_meta(15 downto 2) = (pc(24 downto 12) & "1")) then
+                    if (icache_data = x"0100006F") then 
+                        led <= '0';
+                    else
+                        led <= '1';
                     end if;
-                when S2 =>
-                    icache_load <= '0';
-                    if (icache_meta(15 downto 2) = (pc(24 downto 12) & "1")) then
-                            if (icache_data = x"0100006F") then 
-                                led <= '0';
-                            else
-                                led <= '1';
-                            end if;
-                            state <= S3;
-                    end if;
-                when S3 =>
-            end case;
-        end if;
-    end process;
-    
+                    state <= S3;
+                end if;
+            when S3 =>
+        end case;
+    end if;
+end process;
+
 
 end architecture;
