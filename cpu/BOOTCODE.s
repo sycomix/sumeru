47ce4264d5dc96091ebe5ec3813f821a9e61883e
R0H17 <r0h17@purpe.com>
2019-12-18 02:19:32 +0530

    CSR GPIO implement CSRRS and CSRRC

--- cpu/BOOTCODE.s
+++ cpu/BOOTCODE.s
@@ -4,13 +4,15 @@ main:
     li t0, 1
     li t1, 1
     csrrw zero,0x100,t0
+    csrrw zero,0x103,t0
 mainloop:
     beq t0,t1,vtrue
 vfalse:
-    csrrwi zero,0x103,1
     j vfalse
 vtrue:
-    csrrwi zero,0x103,0
+    addi t0,t0,1
+    srli t1,t0,25
+    csrrw zero,0x103,t1
     j vtrue
 
 .align(8)
