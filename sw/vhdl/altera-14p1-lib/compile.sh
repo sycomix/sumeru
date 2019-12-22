480e8b34f7230b2863416c017739e000af7bdbf5
R0H17 <r0h17@purpe.com>
2019-12-22 15:45:53 +0530

    Adjust altera path to ~/local/altera instead of ~/altera

--- sw/vhdl/altera-14p1-lib/compile.sh
+++ sw/vhdl/altera-14p1-lib/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --altera --vhdl93 --out . --src ~/altera/14.1/quartus/eda/sim_lib
-#~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --altera --vhdl2008 --out . --src ~/altera/14.1/quartus/eda/sim_lib
-~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --cyclone --vhdl93 --out . --src ~/altera/14.1/quartus/eda/sim_lib
-~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --cyclone --vhdl2008 --out . --src ~/altera/14.1/quartus/eda/sim_lib
+#~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --altera --vhdl93 --out . --src $QDIR/eda/sim_lib
+#~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --altera --vhdl2008 --out . --src $QDIR/eda/sim_lib
+#~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --cyclone --vhdl93 --out . --src $QDIR/eda/sim_lib
+~/local/ghdl-0.36-rc1/lib/ghdl/vendors/compile-altera.sh --cyclone --vhdl2008 --out . --src $QDIR/eda/sim_lib
