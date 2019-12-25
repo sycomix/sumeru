e0a802b8a07e538f98b9cbd8cfa0516509165f53
R0H17 <r0h17@purpe.com>
2019-12-25 13:22:40 +0530

    Reduce PLL clock speed to 85 MHz, 100 MHz is causing timing stability problems with the BRANCH instruction

--- cpu_v2/BOOTCODE.s
+++ cpu_v2/BOOTCODE.s
@@ -2,9 +2,9 @@
 _start:
     csrrwi zero,0x100,1
     csrrwi zero,0x103,1
-    li t0,1
-    li t1,0
-    bge t0,t1,vtrue
+    li t0,2
+    li t1,3
+    ble t0,t1,vtrue
 vfalse:
     csrrwi zero,0x103,1
     j vfalse
