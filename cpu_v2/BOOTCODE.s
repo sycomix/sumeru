d3b6e3e8068b1567fa763212c372c08c7e763e40
R0H17 <r0h17@purpe.com>
2019-12-27 21:44:15 +0530

    WIP

--- cpu_v2/BOOTCODE.s
+++ cpu_v2/BOOTCODE.s
@@ -2,29 +2,10 @@
 _start:
     csrrwi zero,0x100,1
     csrrwi zero,0x103,1
-    li x1,0x80
-    li x3,0xdeadc0de
-    sw x3,0(x1)
-
-    lw x2,0(x1)
-    li x1,0x10080
-
-    #0000000 00000 00001 011 00000 0100011
-    #0000 0000 0000 0000 1011 0000 0010 0011
-
-    # DCACHE FLUSH LINE - addr in x1, STORE ifunct3 = 011
-    .word 0x0000B023
-
-    li x1,0x80
-    lw x2,0(x1)
-
-    beq x2,x3,vtrue 
-vfalse:
-    csrrwi zero,0x103,1
-    j vfalse
-vtrue:
-    csrrwi zero,0x103,0
-    j vtrue
-
-.align(8)
-globals:
+    li x1,0
+loop:
+    addi x1,x1,1
+    srli x2,x1,20
+    andi x2,x2,1
+    csrrw zero,0x103,x2
+    j loop
