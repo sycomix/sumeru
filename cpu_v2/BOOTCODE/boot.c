734846f04bf9938649bcffaff2e9ea1672e53af9
R0H17 <r0h17@purpe.com>
2020-01-06 23:17:18 +0530

    INTR ret wip

--- cpu_v2/BOOTCODE/boot.c
+++ cpu_v2/BOOTCODE/boot.c
@@ -1,73 +1,55 @@
 void    _start(void) __attribute__ (( naked ));
 void    _start2(void) __attribute__ (( naked ));
-void    set_gpio_dir(unsigned int x);
-void    set_gpio_out(unsigned int x);
-void    set_timer(unsigned int x);
-#if 0
-int     op_mul(int x, int y);
-int     op_div(int x, int y);
-#endif
 
-void
-_start(void)
-{
-    int i;
-    asm("lui sp, 1");
-    set_gpio_dir(1);
-    set_gpio_out(1);
-    set_timer(0x0000004F);
-
-    i = 0;
-    while (i++ < 0x7ffffff)
-        ;
-    while(1) {
-        asm volatile("csrrwi x0, 0x882, 1;");
-        asm volatile("csrrwi x0, 0x882, 0;");
-    }
-}
-
-void
-_start2(void)
-{
-    asm volatile("csrrwi x0, 0x882, 0;");
-    asm volatile("              \
-        csrrsi a0,0xCC0,0;      \
-        csrrw  x0,0x880,a0;     \
-        nop;                    \
-        nop;                    \
-        csrrwi x0,0x9C0,0;"); 
-    while (1)
-        ;
-}
-
-void
+__attribute__ ((always_inline))
+inline void
 set_timer(unsigned int x)
 {
     asm volatile("csrrw x0, 0x884, %0;" : : "r"(x));
 }
 
-#if 0
-int
-op_mul(int x, int y)
-{
-    return x * y;
-}
-
-int
-op_div(int x, int y)
-{
-    return x / y;
-}
-#endif
-
-void
+__attribute__ ((always_inline))
+inline void
 set_gpio_dir(unsigned int x)
 {
     asm volatile("csrrw x0, 0x881, %0;" : : "r"(x));
 }
 
-void
+__attribute__ ((always_inline))
+inline void
 set_gpio_out(unsigned int x)
 {
     asm volatile("csrrw x0, 0x882, %0;" : : "r"(x));
+}
+
+void
+_start(void)
+{
+    asm("lui sp, 1");
+    set_gpio_dir(1);
+    set_gpio_out(1);
+    set_timer(0x0000004F);
+
+    while(1)
+        ;
+}
+
+void
+_start2(void)
+{
+    asm volatile("              \
+        csrrsi a0,0x882,0;      \
+        xori a0,a0,-1;          \
+        csrrw x0,0x882,a0;      \
+                                \
+                                \
+        csrrsi a0,0xCC0,0;      \
+        csrrw  x0,0x880,a0;     \
+        li a0,0x4f;             \
+        csrrw x0,0x884,a0;      \
+        csrrwi x0,0x9C0,0;"); 
+
+    /* Not reached */
+    while (1)
+        ;
 }
