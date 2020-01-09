61b24744cf06535f48be6e9a909ca3278c4dae17
R0H17 <r0h17@purpe.com>
2020-01-09 22:34:42 +0530

    UART Transmit working at 115200

--- cpu/BOOTCODE/boot.c
+++ cpu/BOOTCODE/boot.c
@@ -3,6 +3,17 @@ void    _start2(void) __attribute__ (( naked ));
 
 __attribute__ ((always_inline))
 inline void
+flush_word(unsigned int x)
+{
+    asm volatile("sw %0, 0(zero);" : : "r"(x));
+    asm volatile(" \
+        li x1,0x10000; \
+        .word 0x0000B023; " : : : "x1");
+
+}
+
+__attribute__ ((always_inline))
+inline void
 set_uart_tx(unsigned int x)
 {
     asm volatile("csrrw x0, 0x886, %0;" : : "r"(x));
@@ -35,7 +46,8 @@ _start(void)
     asm("lui sp, 1");
     set_gpio_dir(1);
     set_gpio_out(1);
-    set_uart_tx(0x00000001);
+    flush_word(0x0a726d73);
+    set_uart_tx(0x00000004);
 
     while(1)
         ;
@@ -52,7 +64,7 @@ _start2(void)
                                 \
         csrrsi a0,0xCC0,0;      \
         csrrw  x0,0x880,a0;     \
-        li a0,0x00000001;       \
+        li a0,0x00000004;       \
         csrrw x0,0x886,a0;      \
         csrrwi x0,0x9C0,0;"); 
 
