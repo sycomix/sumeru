0430a68922eb0b10444c9b717796f1bd3a76f99f
R0H17 <r0h17@purpe.com>
2020-01-20 15:07:35 +0530

    Add sumeru_constants.h : BOOTLOADER WIP

---
+++ cpu/BOOTCODE/sumeru_constants.h
@@ -0,0 +1,23 @@
+#ifndef __SUMERU_CONSTANTS_H
+#define __SUMERU_CONSTANTS_H
+
+#define CSR_REG_GPIO_DIR                0x881
+#define CSR_REG_GPIO_OUTPUT             0x882
+#define CSR_REG_GPIO_INPUT              0xCC1
+
+#define CSR_REG_TIMER_CTRL              0x884
+#define CSR_REG_TIMER_VALUE             0xCC2
+
+#define CSR_REG_CTR_CYCLE               0xC00
+#define CSR_REG_CTR_CYCLE_H             0xC80
+#define CSR_REG_CTR_INSTRET             0xC02
+#define CSR_REG_CTR_INSTRET_H           0xC82
+
+#define CSR_REG_CTX_PCSAVE              0xCC0
+#define CSR_REG_CTX_PCSWITCH            0x880
+#define CSR_REG_SWITCH                  0x9C0
+
+#define CSR_REG_UART0_RX                0x888
+#define CSR_REG_UART0_TX                0x889
+
+#endif
