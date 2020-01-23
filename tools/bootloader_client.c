73ebaa6bf852db1ef3a33f7b2cbc4ea6442836a6
R0H17 <r0h17@purpe.com>
2020-01-23 12:30:07 +0530

    BOOTLOADER CLIENT: tools/bootloader_client.c: WIP

---
+++ tools/bootloader_client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+#include <string.h>
+
+char *code_fname = "a.out";
+char *dev_fname = "/dev/ttyUSB0";
+unsigned char *load_address = (unsigned char *)0x10000;
+unsigned char *jmp_address = (unsigned char *)0x10000;
+
+
+void
+process_cmdline_args(int argc, char **argv)
+{
+    while (1) {
+        switch (getopt(argc, argv, "f:d:a:j:")) {
+            case 'f':
+                strcpy(code_fname, optarg);
+                break;
+            case 'd':
+                strcpy(dev_fname, optarg);
+                break;
+            case 'a':
+                strcpy(load_address, optarg);
+                break;
+            case 'j':
+                strcpy(jmp_address, optarg);
+                break;
+            default:
+                errx(1, "Usage: %s [-f bootcode] [-d device] "
+                        "[-l load_address] [-j jmp_address]\n", argv[0]);
+        }
+    }
+}
+
+
+int
+main(int argc, char **argv)
+{
+    process_cmdline_args(argc, argv);
+    return 0;
+}
+    
