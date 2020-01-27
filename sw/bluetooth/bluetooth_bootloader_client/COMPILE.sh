4e609a399fe5d66cb07cb04435af14d90a93445f
R0H17 <r0h17@purpe.com>
2020-01-27 16:12:42 +0530

    rename COMPILE.sh -> compile.sh

--- sw/bluetooth/bluetooth_bootloader_client/COMPILE.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-(cd contrib/pygattlib; make)
-
-PROG=bluetooth_bootloader_client 
-SRC=${PROG}.c
-
-gcc -o ${PROG} -g -I /usr/include/glib-2.0/gio -I /usr/include/glib-2.0 -I /usr/lib/x86_64-linux-gnu/glib-2.0/include -I /usr/include/bluetooth -I contrib/bluez -I contrib/bluez/btio -I contrib/bluez/lib -I contrib/bluez/attrib -I contrib/bluez/src/shared ${SRC} gatt.c /usr/lib/x86_64-linux-gnu/libbluetooth.a -lglib-2.0 contrib/pygattlib/src/libgatt.so 
