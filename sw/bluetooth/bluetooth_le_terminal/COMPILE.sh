00245b497fbf7cee4ed82b0fa7712c6d9f7a24bf
R0H17 <r0h17@purpe.com>
2020-01-27 15:41:40 +0530

    **bluetooth_bootloader_client working

--- sw/bluetooth/bluetooth_le_terminal/COMPILE.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-(cd contrib/pygattlib; make)
-gcc -o bluetooth_le_terminal -g -I /usr/include/glib-2.0/gio -I /usr/include/glib-2.0 -I /usr/lib/x86_64-linux-gnu/glib-2.0/include -I /usr/include/bluetooth -I contrib/bluez -I contrib/bluez/btio -I contrib/bluez/lib -I contrib/bluez/attrib -I contrib/bluez/src/shared bluetooth_le_terminal.c gatt.c /usr/lib/x86_64-linux-gnu/libbluetooth.a -lglib-2.0 contrib/pygattlib/src/libgatt.so 
-gcc -o bluetooth_le_terminal-file -g -I /usr/include/glib-2.0/gio -I /usr/include/glib-2.0 -I /usr/lib/x86_64-linux-gnu/glib-2.0/include -I /usr/include/bluetooth -I contrib/bluez -I contrib/bluez/btio -I contrib/bluez/lib -I contrib/bluez/attrib -I contrib/bluez/src/shared bluetooth_le_terminal-file.c gatt.c /usr/lib/x86_64-linux-gnu/libbluetooth.a -lglib-2.0 contrib/pygattlib/src/libgatt.so 
