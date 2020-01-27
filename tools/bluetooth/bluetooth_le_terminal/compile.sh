64e6453a37e453412e14f960547c998aee1c2612
R0H17 <r0h17@purpe.com>
2020-01-27 23:24:28 +0530

    Reorganize bluetooth libraries

---
+++ tools/bluetooth/bluetooth_le_terminal/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gcc -o bluetooth_le_terminal -g -I /usr/include/glib-2.0/gio -I /usr/include/glib-2.0 -I /usr/lib/x86_64-linux-gnu/glib-2.0/include -I /usr/include/bluetooth -I ../contrib/bluez -I ../contrib/bluez/btio -I ../contrib/bluez/lib -I ../contrib/bluez/attrib -I ../contrib/bluez/src/shared bluetooth_le_terminal.c ../gatt.c /usr/lib/x86_64-linux-gnu/libbluetooth.a -lglib-2.0 ../contrib/pygattlib/src/libgatt.so 
