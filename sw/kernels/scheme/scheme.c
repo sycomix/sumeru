e5a5492e97fb812d061397ca4ca70c074d5f38e4
R0H17 <r0h17@purpe.com>
2020-02-10 17:28:00 +0530

    K2 scheme intepreter development started

--- sw/kernels/scheme/scheme.c
+++ sw/kernels/scheme/scheme.c
@@ -1,10 +1,34 @@
-unsigned int g_timer_intr_pending;
-unsigned int g_uart0_tx_intr_pending;
-unsigned int g_uart0_rx_intr_pending;
+#include <machine/csr.h>
+#include <machine/uart0.h>
+#include <stdio.h>
+
+#define LINE_MAX        128
+
+int
+read_string(char *buf, unsigned int buf_len)
+{
+    int i;
+
+    --buf_len;
+    for (i = 0; i < buf_len; ++i) {
+        uart0_blocking_read((unsigned char *)(buf + i), 1);
+        if (buf[i] == '\n') {
+            break;
+        }
+    }
+    buf[i] = 0;
+    return i;
+}
 
 
 int
 main(int argc, char **argv, char **envp)
 {
+    char i, buf[LINE_MAX];
+
+    while (1) {
+        i = read_string(buf, LINE_MAX);
+        printf("%d %s\n", i, buf);
+    }
     return 0;
 }
