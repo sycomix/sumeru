9c8539e2c7601df86693755c0d24537e008141e1
R0H17 <r0h17@purpe.com>
2020-02-10 15:05:19 +0530

    Start development of Scheme intepreter

---
+++ sw/kernels/scheme/regex.h
@@ -0,0 +1,6 @@
+#ifndef __REGEX_H
+#define __REGEX_H
+
+int     match(char *regexp, char *text);
+
+#endif 
---
+++ sw/kernels/scheme/scheme.c
@@ -0,0 +1,10 @@
+unsigned int g_timer_intr_pending;
+unsigned int g_uart0_tx_intr_pending;
+unsigned int g_uart0_rx_intr_pending;
+
+
+int
+main(int argc, char **argv, char **envp)
+{
+    return 0;
+}
