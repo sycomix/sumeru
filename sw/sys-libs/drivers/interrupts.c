8c52107c193a588f2cc49e984b2984c5d5c7af88
R0H17 <r0h17@purpe.com>
2020-02-16 16:15:23 +0530

    WIP -- non functional commit, i.e. bug still reproduces -- comment out lines in handle_interrupt

--- sw/sys-libs/drivers/interrupts.c
+++ sw/sys-libs/drivers/interrupts.c
@@ -39,32 +39,16 @@ handle_interrupt(int id)
 {
     switch (id) {
         case INTR_ID_TIMER:
-#if 0
-            if (g_uart0_rx_flags & UART_FLAG_READ_TIMER) {
-                process_rx_data();
-            }
-            if (g_uart0_rx_flags & UART_FLAG_RX_ON)
-                timer_set(UART0_RX_TIMER_TICKS | 0x1);
-            else
-                timer_set(0);
-#endif
             timer_set(0);
             break;
         case INTR_ID_UART0_TX:
             g_uart0_tx_intr_pending = 0;
             break;
-
         case INTR_ID_UART0_RX:
-            //process_rx_data();
-            //g_uart0_rx_pos = 0;
-#if 1
             /* XXX BUG activating nop causes invalid return?? */
+#if 1
             asm("nop");
 #endif
-            //if (g_uart0_rx_flags & UART_FLAG_RX_ON) {
-                //uart0_set_rx(((unsigned int)g_tx_drvbuf_start) | 255);
-            //}
             break;
-#endif
     }
 }
