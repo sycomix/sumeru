725fa60b8b0d540cdd2daf901f111bee206aaafc
R0H17 <r0h17@purpe.com>
2020-02-18 15:07:50 +0530

    driver/interrupts.c: WIP

--- sw/sys-libs/drivers/interrupts.c
+++ sw/sys-libs/drivers/interrupts.c
@@ -43,7 +43,7 @@ handle_interrupt(int id)
             if (g_uart0_rx_flags & UART_FLAG_READ_TIMER)
                 process_rx_data();
             if (g_uart0_rx_flags & UART_FLAG_RX_ON)
-                timer_set(UART0_RX_TIMER_TICKS | 0x1);
+                timer_set(UART0_RX_TIMER_TICKS | 0xf);
             break;
         case INTR_ID_UART0_TX:
             g_uart0_tx_intr_pending = 0;
@@ -52,7 +52,7 @@ handle_interrupt(int id)
             process_rx_data();
             g_uart0_rx_pos = 0;
             if (g_uart0_rx_flags & UART_FLAG_RX_ON) {
-                uart0_set_rx(((unsigned int)g_tx_drvbuf_start) | 255);
+                uart0_set_rx(((unsigned int)g_rx_drvbuf_start) | 255);
             }
             break;
     }
