4bbb63c0e3b00aec05b13941cc62759733381b47
R0H17 <r0h17@purpe.com>
2020-02-20 10:42:23 +0530

    LIBC: Add optimized routines for memcpy,memcmove,strcpy,strlen,memset,strcmp

--- sw/sys-libs/libc/strcpy.c
+++ sw/sys-libs/libc/strcpy.c
@@ -1,20 +1,71 @@
-/*
- * strcpy.c
- *
- * strcpy()
- */
+/* Copyright (c) 2017  SiFive Inc. All rights reserved.
+
+   This copyrighted material is made available to anyone wishing to use,
+   modify, copy, or redistribute it subject to the terms and conditions
+   of the FreeBSD License.   This program is distributed in the hope that
+   it will be useful, but WITHOUT ANY WARRANTY expressed or implied,
+   including the implied warranties of MERCHANTABILITY or FITNESS FOR
+   A PARTICULAR PURPOSE.  A copy of this license is available at
+   http://www.opensource.org/licenses.
+*/
 
 #include <string.h>
+#include <stdint.h>
+
+static __inline unsigned long __libc_detect_null(unsigned long w)
+{
+  unsigned long mask = 0x7f7f7f7f;
+  if (sizeof (long) == 8)
+    mask = ((mask << 16) << 16) | mask;
+  return ~(((w & mask) + mask) | w | mask);
+}
+
 
 char *strcpy(char *dst, const char *src)
 {
-	char *q = dst;
-	const char *p = src;
-	char ch;
+  char *dst0 = dst;
 
-	do {
-		*q++ = ch = *p++;
-	} while (ch);
+  int misaligned = ((uintptr_t)dst | (uintptr_t)src) & (sizeof (long) - 1);
+  if (__builtin_expect(!misaligned, 1))
+    {
+      long *ldst = (long *)dst;
+      const long *lsrc = (const long *)src;
 
-	return dst;
+      while (!__libc_detect_null(*lsrc))
+	*ldst++ = *lsrc++;
+
+      dst = (char *)ldst;
+      src = (const char *)lsrc;
+
+      char c0 = src[0];
+      char c1 = src[1];
+      char c2 = src[2];
+      if (!(*dst++ = c0)) return dst0;
+      if (!(*dst++ = c1)) return dst0;
+      char c3 = src[3];
+      if (!(*dst++ = c2)) return dst0;
+      if (sizeof (long) == 4) goto out;
+      char c4 = src[4];
+      if (!(*dst++ = c3)) return dst0;
+      char c5 = src[5];
+      if (!(*dst++ = c4)) return dst0;
+      char c6 = src[6];
+      if (!(*dst++ = c5)) return dst0;
+      if (!(*dst++ = c6)) return dst0;
+
+out:
+      *dst++ = 0;
+      return dst0;
+    }
+
+  char ch;
+  do
+    {
+      ch = *src;
+      src++;
+      dst++;
+      *(dst - 1) = ch;
+    } while (ch);
+
+  return dst0;
 }
--- sw/sys-libs/libc/strlen.c
+++ sw/sys-libs/libc/strlen.c
@@ -1,13 +1,57 @@
-/*
- * strlen()
- */
+/* Copyright (c) 2017  SiFive Inc. All rights reserved.
+
+   This copyrighted material is made available to anyone wishing to use,
+   modify, copy, or redistribute it subject to the terms and conditions
+   of the FreeBSD License.   This program is distributed in the hope that
+   it will be useful, but WITHOUT ANY WARRANTY expressed or implied,
+   including the implied warranties of MERCHANTABILITY or FITNESS FOR
+   A PARTICULAR PURPOSE.  A copy of this license is available at
+   http://www.opensource.org/licenses.
+*/
 
 #include <string.h>
+#include <stdint.h>
 
-size_t strlen(const char *s)
+
+static __inline unsigned long __libc_detect_null(unsigned long w)
 {
-	const char *ss = s;
-	while (*ss)
-		ss++;
-	return ss - s;
+  unsigned long mask = 0x7f7f7f7f;
+  if (sizeof (long) == 8)
+    mask = ((mask << 16) << 16) | mask;
+  return ~(((w & mask) + mask) | w | mask);
+}
+
+
+size_t strlen(const char *str)
+{
+  const char *start = str;
+
+  if (__builtin_expect ((uintptr_t)str & (sizeof (long) - 1), 0)) do
+    {
+      char ch = *str;
+      str++;
+      if (!ch)
+	return str - start - 1;
+    } while ((uintptr_t)str & (sizeof (long) - 1));
+
+  unsigned long *ls = (unsigned long *)str;
+  while (!__libc_detect_null (*ls++))
+    ;
+  asm volatile ("" : "+r"(ls)); /* prevent "optimization" */
+
+  str = (const char *)ls;
+  size_t ret = str - start, sl = sizeof (long);
+
+  char c0 = str[0 - sl], c1 = str[1 - sl], c2 = str[2 - sl], c3 = str[3 - sl];
+  if (c0 == 0)            return ret + 0 - sl;
+  if (c1 == 0)            return ret + 1 - sl;
+  if (c2 == 0)            return ret + 2 - sl;
+  if (sl == 4 || c3 == 0) return ret + 3 - sl;
+
+  c0 = str[4 - sl], c1 = str[5 - sl], c2 = str[6 - sl], c3 = str[7 - sl];
+  if (c0 == 0)            return ret + 4 - sl;
+  if (c1 == 0)            return ret + 5 - sl;
+  if (c2 == 0)            return ret + 6 - sl;
+
+  return ret + 7 - sl;
 }
