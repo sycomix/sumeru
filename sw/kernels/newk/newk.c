3eeec9f07713ca45e4436b1713a09343df6d4218
R0H17 <r0h17@purpe.com>
2020-02-23 13:21:21 +0530

    Bugwork WIP

--- sw/kernels/newk/newk.c
+++ sw/kernels/newk/newk.c
@@ -10,7 +10,6 @@
 int
 main(int argc, char *argv)
 {
-    //unsigned char *buf;
     volatile unsigned int i;
 
     i = 0;
@@ -19,13 +18,9 @@ main(int argc, char *argv)
 
     gpio_set_out(0);
     gpio_set_dir(1);
-    //buf = (unsigned char*) malloc(128);
     uart0_start_engine();
     while (1) {
-        //printf("Hello World");
         uart0_blocking_write("Hello World", 11);
-        //fgets(buf, 128, stdin);
-        //fputs(buf, stdout);
     }
     return 0;
 }
