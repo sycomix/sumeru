2f11b4f2073cd03ee23017fbca003adaba45f8a5
R0H17 <r0h17@purpe.com>
2020-03-05 16:20:54 +0530

    Consprod receive WIP

--- sw/include/machine/uart0.h
+++ sw/include/machine/uart0.h
@@ -3,8 +3,11 @@
 
 #include <machine/consprod.h>
 
-extern consprod_t       g_tx_cp;
-extern consprod_t       g_rx_cp;
+#define UART0_ENGINE_ON         (1 << 0)
+
+extern consprod_t       g_uart0_tx_cp;
+extern consprod_t       g_uart0_rx_cp;
+extern unsigned int     g_uart0_flags;
 
 void    uart0_start_engine();
 void    uart0_stop_engine();
